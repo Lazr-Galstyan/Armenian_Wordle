a10af5228d5807012d8200dd43a0d22178f27311
Lazr <lazrgalstyan@gmail.com>
2025-03-16 23:22:19 +0000

    corrected issue with icons

--- app.R
+++ app.R
@@ -219,6 +219,9 @@ ui <- fluidPage(
   ),
   selectInput(inputId = 'attempts', label = 'Փորձերի Քանակ', c(6, 7, 8, 9, 10)),
   align = "center", textOutput("attemptstext"),
+  tags$head(
+    tags$link(rel = "stylesheet", href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css")
+  ),
   div(
     tagAppendAttributes(
       textInput("guess", "", placeholder = "Գրել Այստեղ"),
